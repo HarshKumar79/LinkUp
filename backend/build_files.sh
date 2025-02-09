ecb3149ffd60b15082fc3a0296461ccbae209519
HarshKumar79 <harsh751883@gmail.com>
2025-02-09 20:04:16 +0530

    Update build_files.sh

--- backend/build_files.sh
+++ backend/build_files.sh
@@ -4,7 +4,4 @@ pip install -r backend/requirements.txt
 echo "Applying migrations..."
 python backend/manage.py migrate --noinput
 
-echo "Collecting static files..."
-python backend/manage.py collectstatic --noinput --clear
-
 echo "Build completed successfully."
